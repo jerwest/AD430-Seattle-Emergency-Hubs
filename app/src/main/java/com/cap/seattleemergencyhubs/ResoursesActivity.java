b7b114dee64f5294f214657abd89d77583c39b0b
Captain <44041524+canaimax@users.noreply.github.com>
2019-11-06 16:29:10 -0800

    Update ResoursesActivity.java

--- app/src/main/java/com/cap/seattleemergencyhubs/ResoursesActivity.java
+++ app/src/main/java/com/cap/seattleemergencyhubs/ResoursesActivity.java
@@ -3,10 +3,12 @@ package com.cap.seattleemergencyhubs;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class ResoursesActivity extends AppCompatActivity {
 

ba86ff0c29c3ef334429c9174445af96865fbe1a
Iryna <iryna.sherepot@gmail.com>
2019-11-06 21:51:45 -0800

    cleaned  up

--- app/src/main/java/com/cap/seattleemergencyhubs/HubsListAdapter.java
+++ app/src/main/java/com/cap/seattleemergencyhubs/HubsListAdapter.java
@@ -22,6 +22,9 @@ public class HubsListAdapter extends ArrayAdapter<Hub> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.hub_row_item_view, parent, false);
         }
+
+
+
         //ImageView imageView = findViewById(R.id.movieImage);
         //String url = "movies"
         Hub hub = getItem(position);
@@ -40,29 +43,4 @@ public class HubsListAdapter extends ArrayAdapter<Hub> {
         return convertView;
     }
 
-/*
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_list_row_item, parent, false);
-        }
-        //ImageView imageView = findViewById(R.id.movieImage);
-        //String url = "movies"
-
-        String[] movie = getItem(position);
-        TextView movieTitle = (TextView) convertView.findViewById(R.id.movieTitle);
-        movieTitle.setText(movie[0]);
-
-        TextView movieYear = (TextView) convertView.findViewById(R.id.movieYear);
-        movieYear.setText(movie[1]);
-
-        ImageView movieImage = convertView.findViewById(R.id.movieImage);
-        String url = (movie[3]);
-
-        Picasso.get().load(url).into(movieImage);
-
-        return convertView;
-    }
-
-*/
 }
--- app/src/main/java/com/cap/seattleemergencyhubs/SelectedNeighborhoods.java
+++ app/src/main/java/com/cap/seattleemergencyhubs/SelectedNeighborhoods.java
@@ -1,28 +1,20 @@
 package com.cap.seattleemergencyhubs;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
-
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class SelectedNeighborhoods extends AppCompatActivity {
 
-    private ArrayList<Hub> currentNeighborhoodHubs = new ArrayList<>(); // empty at the time
+    private ArrayList<Hub> currentNeighborhoodHubs = new ArrayList<>();
     private static final String TAG = "Neighborhoods Activity";
 
     @Override
@@ -41,16 +33,11 @@ public class SelectedNeighborhoods extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.neighborhood_map);
 
-
         Button firstNeighborhood = (Button) findViewById(R.id.first_neighborhood);
         Button secondNeighborhood = (Button) findViewById(R.id.second_neighborhood);
         Button thirdNeighborhood = (Button) findViewById(R.id.third_neighborhood);
         FrameLayout editButton = (FrameLayout) findViewById(R.id.Add_edit_button);
-
-
-
         // second part - a list view of hubs
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("My Default Neighborhoods");
@@ -62,13 +49,7 @@ public class SelectedNeighborhoods extends AppCompatActivity {
         final ListView listView = (ListView) findViewById(R.id.hubs_list);
         listView.setAdapter(adapter);
 
-
-
-
     }
-
-
-
 
     @Override
     public void onStart() {

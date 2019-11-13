6d3789dea4a98324cdbcc1d8835fbfb9b44434f8
Yozelinm <yozelin.m_206@yahoo.com>
2019-11-12 23:45:13 -0800

    Faqlayout
    Faq layout, floating action button dependecies added and text views added

---
+++ app/src/main/java/com/cap/seattleemergencyhubs/FaqHUBS.java
@@ -0,0 +1,14 @@
+package com.cap.seattleemergencyhubs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaqHUBS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq_hubs);
+    }
+}

5d19e2bee1dd65c3a60fbe6140984adffcb70ec8
Iryna <iryna.sherepot@gmail.com>
2020-03-26 18:41:01 -0700

    updated dependencies to newer versions

--- app/src/main/java/com/cap/seattleemergencyhubs/SelectedNeighborhoods.java
+++ app/src/main/java/com/cap/seattleemergencyhubs/SelectedNeighborhoods.java
@@ -49,7 +49,7 @@ public class SelectedNeighborhoods extends AppCompatActivity {
 
     private ArrayList<Hub> currentNeighborhoodHubs = new ArrayList<>();
     private static final String TAG = "Neighborhoods Activity";
-    private String firstSelectedNeighbName;
+    private String firstSelectedNeighbName = currentNeighborhoodHubs.get(0).getNeighborhood().toUpperCase();
     Button popUpMapLegendButton, closePopupBtn;
     PopupWindow popupWindow;
     ScrollView linearLayout1;
@@ -86,7 +86,6 @@ public class SelectedNeighborhoods extends AppCompatActivity {
             currentNeighborhoodHubs = (ArrayList<Hub>) bundle.getSerializable("neighborhoodName");
             Log.i("ArrayList ", currentNeighborhoodHubs.toArray().toString());
             if (currentNeighborhoodHubs != null) {
-                firstSelectedNeighbName = currentNeighborhoodHubs.get(0).getNeighborhood().toUpperCase();
                 Log.i(" *** Current hubs list ", firstSelectedNeighbName);
             }
         }
